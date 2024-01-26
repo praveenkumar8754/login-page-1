a6dab1d88d786f5db670e1e0f288d5382defdcaf
praveenkumar8754 <praveenraman7877@gmail.com>
2024-01-26 14:17:52 +0530

    code updated

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ function statusChangeCallback(response) {
 document.getElementById('facebook-login').addEventListener('click', function() {
   FB.login(checkLoginState, { scope: 'email' });
 });
-
-// Load the SDK asynchronously
 // Load the SDK asynchronously
 (function(d, s, id) {
 var js, fjs = d.getElementsByTagName(s)[0];
@@ -35,38 +33,41 @@ js = d.createElement(s); js.id = id;
 js.src = "https://connect.facebook.net/en_US/sdk.js";
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-
-
+const localAccessToken = "newAccessToken";
 
 
 //sign in with google
 
-function handleCredentialResponse(response){
-	console.log(JSON.stringify(response));
-	
-	var token =  response.credential;
+function handleCredentialResponse(response) {
+  console.log(JSON.stringify(response));
+  var token = response.credential;
 
+  var parts = token.split('.');
+  var decodedPayload = JSON.parse(atob(parts[1]));
+  console.log(decodedPayload);
 
-var parts = token.split('.');
-var decodedPayload = JSON.parse(atob(parts[1]));
-console.log(decodedPayload);
-	
-	 
- 
-            let responsePayload = decodedPayload;
+  let responsePayload = decodedPayload;
 
-            
-            let profileHTML = '<h3>Welcome '+responsePayload.given_name+'! <a href="javascript:void(0);" onclick="signOut('+responsePayload.sub+');">Sign out</a></h3>';
-            profileHTML += '<img src="'+responsePayload.picture+'"/><p><b>Auth ID: </b>'+responsePayload.sub+'</p><p><b>Name: </b>'+responsePayload.name+'</p><p><b>Email: </b>'+responsePayload.email+'</p>';
-            document.getElementsByClassName("pro-data")[0].innerHTML = profileHTML;
-            
-            document.querySelector("#btnWrap").classList.add("hidden");
-            document.querySelector(".pro-data").classList.remove("hidden");
-			
- 
+  let profileHTML = '<h3>Welcome ' + responsePayload.given_name + '! <a href="javascript:void(0);" onclick="signOut(' + responsePayload.sub + ');">Sign out</a></h3>';
+  profileHTML += '<img src="' + responsePayload.picture + '"/><p><b>Auth ID: </b>' + responsePayload.sub + '</p><p><b>Name: </b>' + responsePayload.name + '</p><p><b>Email: </b>' + responsePayload.email + '</p>';
+
+  let proDataElement = document.getElementsByClassName("pro-data")[0];
+  if (proDataElement) {
+      proDataElement.innerHTML = profileHTML;
+  } else {
+      console.error('Element with class "pro-data" not found!');
+  }
+
+  document.querySelector("#btnWrap").classList.add("hidden");
+  document.querySelector(".pro-data").classList.remove("hidden");
 }
+
 function signOut(authID) {
-  document.getElementsByClassName("pro-data")[0].innerHTML = '';
+  let proDataElement = document.getElementsByClassName("pro-data")[0];
+  if (proDataElement) {
+      proDataElement.innerHTML = '';
+  }
+
   document.querySelector("#btnWrap").classList.remove("hidden");
   document.querySelector(".pro-data").classList.add("hidden");
-}    
+}
